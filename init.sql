f03a2897fb0e14c12fcbcc1e01bdacb0bd2faa28
NivethaM <119606920+weluvpandaz@users.noreply.github.com>
2024-06-14 21:09:59 +0530

    Create init.sql

---
+++ init.sql
@@ -0,0 +1,9 @@
+-- init.sql
+
+CREATE TABLE IF NOT EXISTS "FormSubmissions" (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  usn VARCHAR(50),
+  ip_address VARCHAR(50) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
