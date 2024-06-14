81489c273b3530c89d587d6e2323990f43ca8fec
NivethaM <119606920+weluvpandaz@users.noreply.github.com>
2024-06-14 19:24:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,12 +23,7 @@ const connectionString = process.env.DB_URI_INTERNAL;
 
 // PostgreSQL database connection setup
 const client = new Client({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  connectionString
+  connectionString: process.env.DB_URI_INTERNAL
 });
 
 client.connect()
