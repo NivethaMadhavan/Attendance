7e66a2ee31e7f3e282e301400de9554a491e5958
NivethaM <119606920+weluvpandaz@users.noreply.github.com>
2024-07-13 16:12:12 +0530

    Update home.js

--- home.js
+++ home.js
@@ -355,7 +355,7 @@ app.post('/register', async (req, res) => {
     const insertQuery2 = `
       INSERT INTO login_credentials (student_id, password_hash)
       VALUES ($1, $2)
-      ON CONFLICT (usn) DO NOTHING;
+      ON CONFLICT (student_id) DO NOTHING;
     `;
     await client.query(insertQuery2, [usn, password]);
     
